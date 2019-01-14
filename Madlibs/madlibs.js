a9b24c2cfaf6911c4d6149daac66eb0529b429bb
unknown <beniciva000@warren.k12.in.us>
2019-01-14 13:16:27 -0500

    start

---
+++ Madlibs/madlibs.js
@@ -0,0 +1,23 @@
+var color
+var superlative
+var adj
+var bodyPt
+var bodyPt2
+var noun
+var animal
+var adj1
+var adj2
+var adj3
+
+function getInfo() {
+    color = $("#color").val();
+    superlative = $("#superlative").val();
+    adj = $("#adj").val();
+    bodyPt = $("#bodyPt").val();
+    bodyPt2 = $("#bodyPt2").val();
+    noun = $("#noun").val();
+    animal = $("#animal").val();
+    adj1 = $("#adj1").val();
+    adj2 = $("#adj2").val();
+    adj3 = $("#adj3").val();
+}
